09ae6f3d73a061c92e81bc0d4dcf69e5aecfbfdf
Maqed <magedibra@yahoo.com>
2025-06-20 08:43:05 +0300

    fix: dark mode border color

--- src/components/pages/standings-page.tsx
+++ src/components/pages/standings-page.tsx
@@ -50,7 +50,7 @@ function StandingsPage() {
   return (
     <>
       {/* Header */}
-      <header className="border-b border-gray-200">
+      <header className="border-b border-gray-200 dark:border-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-4">
