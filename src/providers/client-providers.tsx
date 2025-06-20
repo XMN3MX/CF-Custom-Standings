33706b5a4ba49a3bfbde18c3c4cfb22e213cb6c8
Maqed <magedibra@yahoo.com>
2025-06-20 08:18:12 +0300

    feat: add react query

--- src/providers/client-providers.tsx
+++ src/providers/client-providers.tsx
@@ -1,7 +1,11 @@
+"use client";
 import React, { ReactNode } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/get-query-client";
 
 function ClientProviders({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
   return (
     <NextThemesProvider
       attribute="class"
@@ -9,7 +13,7 @@ function ClientProviders({ children }: { children: ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </NextThemesProvider>
   );
 }
